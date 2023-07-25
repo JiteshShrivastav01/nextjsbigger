00082386c9d1ba4ea99f6efcfb43d28b8e84ba04
JiteshShrivastav01 <jitesh.cs01@gmail.com>
2023-07-25 19:48:56 +0530

    Task 5

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,15 @@
+const Meetup = (props) => {
+  return (
+    <section>
+      <img
+        src="https://th.bing.com/th/id/OIP.yQV9YN97q7-pD-MDLHFaJAHaEK?pid=ImgDet&rs=1"
+        alt=""
+        style={{ width: "400px" }}
+      />
+      <address>Place 1 ,City 3</address>
+      <p>Title</p>
+    </section>
+  );
+};
+
+export default Meetup;
