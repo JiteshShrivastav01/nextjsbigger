9054074bebdcfddbb7673ef3d5580d19e04e3b3d
JiteshShrivastav01 <jitesh.cs01@gmail.com>
2023-07-25 20:46:23 +0530

    Task 6

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -12,4 +12,40 @@ const Meetup = (props) => {
   );
 };
 
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetUpData: {
+        image:
+          "https://th.bing.com/th/id/OIP.yQV9YN97q7-pD-MDLHFaJAHaEK?pid=ImgDet&rs=1",
+        id: meetupId,
+        title: "The place 3",
+        address: "Place 3 , city 3.",
+        description: "abcdefghijklmnopqr",
+      },
+    },
+  };
+}
+
 export default Meetup;
--- pages/index.js
+++ pages/index.js
@@ -13,12 +13,24 @@ const Dummy_items = [
     title: "Place 2",
     image:
       "https://th.bing.com/th/id/R.0af9cf9713f5c23dfc432fbdb416aafc?rik=dQPuvuDStQ02Nw&riu=http%3a%2f%2fstatic6.businessinsider.com%2fimage%2f51c9df8c69bedd794c00000d%2fthe-25-most-popular-tourist-attractions-in-the-world.jpg&ehk=OstwvbL%2fgryABVDTY1lw10fxo38rvUujCusgq32YRW4%3d&risl=&pid=ImgRaw&r=0",
-    address: "Plac2 ,city 1",
+    address: "Place2 ,city 1",
   },
 ];
 
-const HomePage = () => {
-  return <MeetupList meetups={Dummy_items} />;
+const HomePage = (props) => {
+  return (
+    <>
+      <MeetupList meetups={props.meetups} />
+    </>
+  );
 };
+
+export async function getStaticProps() {
+  return {
+    props: {
+      meetups: Dummy_items,
+    },
+  };
+}
 
 export default HomePage;
